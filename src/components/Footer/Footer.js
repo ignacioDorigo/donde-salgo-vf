8ad51433df3bcd6833d864c5147c6b6741fe641f
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-05-20 01:39:36 -0300

    footer pasado

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,83 @@
+import React from "react";
+import "./Footer.css";
+import Logo from "../Logo/Logo";
+
+export default function Footer() {
+  return (
+    <div className="footer__bg">
+      <footer className="footer contenedor">
+        <Logo className="footer__logo" />
+        <div className="div__redes">
+          {/* Tiktok */}
+          <a
+            href="https://www.tiktok.com/foryou"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg
+              className="svg__redes"
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#E1E1E1"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M21 7.917v4.034a9.948 9.948 0 0 1 -5 -1.951v4.5a6.5 6.5 0 1 1 -8 -6.326v4.326a2.5 2.5 0 1 0 4 2v-11.5h4.083a6.005 6.005 0 0 0 4.917 4.917z" />
+            </svg>
+          </a>
+
+          {/* Twitter */}
+          <a
+            href="https://x.com/home"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg
+              className="svg__redes"
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#E1E1E1"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
+              <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
+            </svg>
+          </a>
+
+          {/* Instagram */}
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg
+              className="svg__redes"
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#E1E1E1"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+              <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+              <path d="M16.5 7.5l0 .01" />
+            </svg>
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import MainHome from "../components/MainHome/MainHome";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <MainHome />
+      <Footer />
     </div>
   );
 }
