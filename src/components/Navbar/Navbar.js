70a2da6158364b20b376e1d843beabd66640c3bb
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-05-20 02:24:43 -0300

    navbar responsive listo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Logo/Logo";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const [menuAbierto, setMenuAbierto] = useState(false);
+
   return (
     <div className="nav_bg">
       <nav className="nav contenedor">
         <Logo />
+
+        <div
+          className="hamburguesa"
+          onClick={() => setMenuAbierto(!menuAbierto)}
+        >
+          ☰
+        </div>
+
+        <div
+          className={`menu_mobile_overlay ${
+            menuAbierto ? "menu_mobile_visible" : ""
+          }`}
+        >
+          <p className="enlaces" onClick={() => setMenuAbierto(false)}>
+            Ingresar
+          </p>
+          <p
+            className="enlaces crearCuenta"
+            onClick={() => setMenuAbierto(false)}
+          >
+            Crear cuenta
+          </p>
+        </div>
+
+        {/* Escritorio */}
         <div className="div__nav__enlaces">
-          {/* Hay que usar react router */}
           <p className="enlaces">Ingresar</p>
           <p className="enlaces crearCuenta">Crear cuenta</p>
         </div>
