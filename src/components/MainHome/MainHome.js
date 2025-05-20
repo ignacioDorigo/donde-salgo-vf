0f15cddf6e57ccae6c5aa170626bd7f94aa743db
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-05-20 02:30:47 -0300

    main responsive listo

--- src/components/MainHome/MainHome.js
+++ src/components/MainHome/MainHome.js
@@ -12,7 +12,24 @@ export default function MainHome() {
           id="buscar"
           placeholder="Buscar eventos o lugares"
         />
-        <button className="btn_buscar">Buscar</button>
+        <button className="btn_buscar">
+          <span className="texto-btn">Buscar</span>
+          <svg
+            className="icono-lupa"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#FFFFFF"
+            stroke-width="1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+            <path d="M21 21l-6 -6" />
+          </svg>
+        </button>
       </div>
     </div>
   );
