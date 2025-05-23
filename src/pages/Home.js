e5f77883ac17d91efa51ac16e8cb52fa25e75a0e
Federico Rosciani <103347032+FedericoRosciani@users.noreply.github.com>
2025-05-23 16:05:35 -0300

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,14 @@ import Footer from "../components/Footer/Footer";
 import MainHome from "../components/MainHome/MainHome";
 import Categorias from "../components/Categorias/Categorias";
 import Eventos from "../components/Eventos/Eventos";
+import RegistroPromo from "../components/RegistroPromo/RegistroPromo";
+import RecomendadosPorZona from "../components/RecomendadosPorZona/RecomendadosPorZona";
+import SeparadorLinea from "../components/linea/SeparadorLinea";
+
+
+
+
+
 
 export default function Home() {
   return (
@@ -12,6 +20,8 @@ export default function Home() {
       <MainHome />
       <Categorias />
       <Eventos />
+      <RecomendadosPorZona />
+      <RegistroPromo />
       <Footer />
     </div>
   );
